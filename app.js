72229525458b778538bc4a8e71b1a242fa35ac4d
Katniss Everdeen <jan.bentsche@googlemail.com>
2019-01-12 19:02:02 +0100

    Working State

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var bodyParser = require('body-parser');
+var tediousExpress = require('express4-tedious');
+var config = require('config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,21 +31,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    req.sql = tediousExpress(config.get('connection'));
+    next();
 });
 
+app.use(bodyParser.text());
+app.use('/mitglied', require('./routes/mitglied/mitglieder'));
+
+app.use(bodyParser.text());
+app.use('/admin', require('./routes/admin/admin'));
+
+app.use(bodyParser.text());
+app.use('/dienst', require('./routes/dienst/dienste'));
+
+app.use(bodyParser.text());
+app.use('/gs', require('./routes/gutscheine/gs'));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    /* var err = new Error('Not Found: '+ req.method + ":" + req.originalUrl);
+     err.status = 404;
+     next(err);*/
+    next(createError(404));
+});
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
